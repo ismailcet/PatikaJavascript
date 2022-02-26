f5be5afa8a6450a10a29ddff6b7fd2ebfd5abf25
ismailcet <ismailcet3@gmail.com>
2022-02-26 20:40:01 +0300

    Fonksiyonlar - Bölüm Sonu Egzersizi

--- Fonksiyonlar - Bölüm Sonu Egzersizi/script.js
+++ Fonksiyonlar - Bölüm Sonu Egzersizi/script.js
@@ -1,15 +1,28 @@
-let counter=0;
-let number=document.querySelector(".number");
+let value=document.querySelector('#value');
+let decrease=document.querySelector('.decrease');
+let reset=document.querySelector('.reset');
 let increase=document.querySelector('.increase');
-let decraese=document.querySelector('.decrease');
 
-
-
+let counter=0;
 
 increase.addEventListener('click',eventClick);
-decraese.addEventListener('click',eventClick);
+decrease.addEventListener('click',eventClick);
+reset.addEventListener('click',function(){
+    counter=0;
+    value.innerText=counter;
+    value.style.color='#102a42';
+})
 
 function eventClick(){
-    this.className==='increase' ? counter++: counter--;
-    number.innerText=counter;
+    this.className.includes('increase') ?counter++:counter--;
+    value.innerText=counter;
+    if(counter>0){
+        value.style.color='green';
+    }
+    else if(counter <0){
+        value.style.color='red';
+    }
+    else{
+        value.style.color='#102a42'
+    }
 }
