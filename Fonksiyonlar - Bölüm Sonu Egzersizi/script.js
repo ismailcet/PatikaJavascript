8a6277e1bffe87ece61c20f3da45bb0fedf10229
ismailcet <ismailcet3@gmail.com>
2022-02-26 19:37:45 +0300

    Fonksiyonlar - Bölüm Sonu Egzersizi

--- Fonksiyonlar - Bölüm Sonu Egzersizi/script.js
+++ Fonksiyonlar - Bölüm Sonu Egzersizi/script.js
@@ -3,8 +3,13 @@ let number=document.querySelector(".number");
 let increase=document.querySelector('.increase');
 let decraese=document.querySelector('.decrease');
 
-function evenClick(){
 
+
+
+increase.addEventListener('click',eventClick);
+decraese.addEventListener('click',eventClick);
+
+function eventClick(){
+    this.className==='increase' ? counter++: counter--;
+    number.innerText=counter;
 }
-
-number.innerText=counter;
