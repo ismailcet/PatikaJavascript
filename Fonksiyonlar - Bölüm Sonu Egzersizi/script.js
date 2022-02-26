d5eaf92b91fbc80a8490f7a7e5f27a0d70f96df7
ismailcet <ismailcet3@gmail.com>
2022-02-26 19:24:45 +0300

    Fonksiyonlar - Bölüm Sonu Egzersizi

---
+++ Fonksiyonlar - Bölüm Sonu Egzersizi/script.js
@@ -0,0 +1,10 @@
+let counter=0;
+let number=document.querySelector(".number");
+let increase=document.querySelector('.increase');
+let decraese=document.querySelector('.decrease');
+
+function evenClick(){
+
+}
+
+number.innerText=counter;
