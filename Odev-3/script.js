11d7ae949dad8727a8583f3ad26fa743d1c77cd5
ismailcet <ismailcet3@gmail.com>
2022-03-02 16:16:33 +0300

    Odev-3

--- Odev-3/script.js
+++ Odev-3/script.js
@@ -2,83 +2,151 @@ const menu = [
     {
       id: 1,
       title: "buttermilk pancakes",
-      category: "breakfast",
+      category: "Breakfast",
       price: 15.99,
-      img: "./img/item-1.jpeg",
+      img: "img/item-1.jpeg",
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
       id: 2,
       title: "diner double",
-      category: "lunch",
+      category: "Lunch",
       price: 13.99,
-      img: "./img/item-2.jpeg",
+      img: "img/item-2.jpeg",
       desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
       id: 3,
       title: "godzilla milkshake",
-      category: "shakes",
+      category: "Shakes",
       price: 6.99,
-      img: "./img/item-3.jpeg",
+      img: "img/item-3.jpeg",
       desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
     {
       id: 4,
       title: "country delight",
-      category: "breakfast",
+      category: "Breakfast",
       price: 20.99,
-      img: "./img/item-4.jpeg",
+      img: "img/item-4.jpeg",
       desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
     },
     {
       id: 5,
       title: "egg attack",
-      category: "lunch",
+      category: "Lunch",
       price: 22.99,
-      img: "./img/item-5.jpeg",
+      img: "img/item-5.jpeg",
       desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
     },
     {
       id: 6,
       title: "oreo dream",
-      category: "shakes",
+      category: "Shakes",
       price: 18.99,
-      img: "./img/item-6.jpeg",
+      img: "img/item-6.jpeg",
       desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
     },
     {
       id: 7,
       title: "bacon overflow",
-      category: "breakfast",
+      category: "Breakfast",
       price: 8.99,
-      img: "./img/item-7.jpeg",
+      img: "img/item-7.jpeg",
       desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
     },
     {
       id: 8,
       title: "american classic",
-      category: "lunch",
+      category: "Lunch",
       price: 12.99,
-      img: "./img/item-8.jpeg",
+      img: "img/item-8.jpeg",
       desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
     },
     {
       id: 9,
       title: "quarantine buddy",
-      category: "shakes",
+      category: "Shakes",
       price: 16.99,
-      img: "./img/item-9.jpeg",
+      img: "img/item-9.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
     {
       id: 10,
       title: "steak dinner",
-      category: "dinner",
+      category: "Dinner",
       price: 39.99,
-      img: "./img/item-10.jpeg",
+      img: "img/item-10.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
   ];
 
-  
+const menuItems=document.querySelector('.items-content');
+const buttons=document.querySelector('.filter');
+
+//Function
+
+function displayItems(){
+    for(let i=0;i<menu.length;i++){
+        let html=`      <article class="menu-item">
+                            <img src="${menu[i].img}" alt="photo${menu[i].id}" class="photo">
+                            <div class="item-info">
+                                <div class="item-title">
+                                    <h4>${menu[i].title}</h4>
+                                    <h4 class="price">$${menu[i].price}</h4>
+                                </div>
+                                <p class="info">${menu[i].desc}</p>
+                            </div>
+                        </article>`
+        menuItems.innerHTML +=html;        
+    }
+} 
+
+function filterItem(category){
+    menuItems.innerHTML='';
+    let html;
+    let newMenu=menu.filter(item=>item.category===category);
+    console.log(newMenu);
+    for(let i=0;i<newMenu.length;i++){
+        html=`      <article class="menu-item">
+                        <img src="${newMenu[i].img}" alt="photo${newMenu[i].id}" class="photo">
+                        <div class="item-info">
+                            <div class="item-title">
+                                <h4>${newMenu[i].title}</h4>
+                                <h4 class="price">$${newMenu[i].price}</h4>
+                            </div>
+                            <p class="info">${newMenu[i].desc}</p>
+                        </div>
+                    </article>`
+        menuItems.innerHTML +=html;
+    }
+}
+
+
+//Filter Display Function
+function filterDisplay(e){
+    let category=e.target.attributes['data-id'].value
+    if(category==='all'){
+        menuItems.innerHTML='';
+        displayItems();
+    }
+    if(category==='Breakfast'){
+        filterItem(category);
+    }
+    if(category==='Lunch'){
+        filterItem(category);
+    }
+    if(category==='Shakes'){
+        filterItem(category);
+    }
+    if(category==='Dinner'){
+        filterItem(category);
+    }
+}
+
+
+
+//Event Listeners
+window.addEventListener('DOMContentLoaded',displayItems);
+buttons.addEventListener('click',filterDisplay);
+
